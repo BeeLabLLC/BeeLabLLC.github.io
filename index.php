b1e4567f47c3f056a79f888f584faa9334cce042
Masum Hasan <masumhasan92@gmail.com>
2019-10-26 03:34:46 +0600

    index

---
+++ index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en">
+ 
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+	<meta name="Description" content="THE BREEDING GROUND OF IDEA
+WHERE CREATIVITY
+MEETS TECHNOLOGY" />
+	<meta content="IE=edge" http-equiv="X-UA-Compatible" />
+	<base  />
+
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+	
+	<meta name="format-detection" content="telephone=no" />
+	<meta name="robots" content="index,follow" />	
+	<link rel="icon" type="image/png" href="https://raw.githubusercontent.com/BeeLabLLC/websitefile/master/beelab-fav.png">
+	
+	<link rel="stylesheet" type="text/css" href="https://firebasestorage.googleapis.com/v0/b/beelab-inc.appspot.com/o/rootsite%2Fcss%2Fbase.css?alt=media&token=2025c717-4840-44fc-80e7-e59829595aaf" />
+	
+	<link type="text/css" rel="stylesheet" href="https://firebasestorage.googleapis.com/v0/b/beelab-inc.appspot.com/o/rootsite%2Fcss%2Findex.css?alt=media&token=9c8f3868-1fc9-437a-8bc8-435e972e0e69">
+
+	
+	<link href='https://fonts.googleapis.com/css.6aa71.delayed?family=PT+Sans:400,700,400italic&amp;subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+	<script type="text/javascript" src="https://firebasestorage.googleapis.com/v0/b/beelab-inc.appspot.com/o/rootsite%2Fjs%2Fmodernizr.custom.js?alt=media&token=59d9bbba-7ec3-42ec-aa9f-e7add0679a80"></script>
+	
+	
+
+
+
+	<!--[if lt IE 9]>
+	<script>
+		document.createElement('header');
+		document.createElement('nav');
+		document.createElement('section');
+		document.createElement('article');
+		document.createElement('aside');
+		document.createElement('footer');
+		document.createElement('hgroup');
+	</script>
+	<![endif]-->
+	
+ 	<title>BeeLab LLC.</title>
+</head>
+
+<body  class='index-page unloaded'>
+
+	
+	<img class="loadBackground" src="https://i.imgur.com/N5shEsT.jpg" alt="" />
+
+	
+	<div class='Preloader text-center'>
+		<span class='Preload-end'><img class="Preload-endImg" src="https://raw.githubusercontent.com/BeeLabLLC/beelab.github.io/master/BeeLabLLC-logo-load.png" alt="" /></span><span class='Preload-start'><img class="Preload-startImg" src="https://raw.githubusercontent.com/BeeLabLLC/beelab.github.io/master/BeeLabLLC-logo-d.png" alt="" /></span>
+	</div>
+
+	<div id="parallax" class="Parallax">
+		
+		<img data-speed="0.1" class="parallaxBackground" src="https://raw.githubusercontent.com/BeeLabLLC/websitefile/master/bck.png" alt="" />
+
+		
+		<div id="intro">
+			
+			<img src="https://i.imgur.com/EJP8Hf6.png" class="intro-pic intro-1stImg" alt="" />
+			<img data-speed="1.5" src="https://i.imgur.com/12hn8DI.png" class="intro-pic" alt="" />
+			<img data-speed="2.5" src="https://i.imgur.com/pLybQpL.png" class="intro-pic" alt="" />
+			
+			<div class="fullScreen topCaption" data-speed="4">
+				<h1>Web development<br>at Dot where creativity<br>meets technology</h1>
+			</div>
+			
+			<img data-speed="5.5" class='test-subject' src="https://i.imgur.com/FW9zFUs.png" alt="" />
+		</div>
+		
+		
+		<div id="bcode">
+			
+			<div class="fullScreen" data-speed="1">
+				<div class="table">
+					<div class="cell">
+						<img src="https://i.imgur.com/VnjW2rJ.png" alt="" class="plLayer bcodeLayer" />
+					</div>
+				</div>
+			</div>
+			
+			<div class="topCaption">
+				<h2 data-speed="2.5">
+					<a href="#">Website & logo<br/>for clinic</a>
+				</h2>
+			</div>
+			
+			<div class="fullScreen text-center" data-speed="4">
+				<div class="table">
+					<div class="cell">
+						<span class="bcode-logo">BCODE Beauty</span>
+					</div>
+				</div>
+			</div>
+			<div class="topCaption link-shadow">
+				<div class="h1" data-speed="2.5">
+					<a href="#">Website & logo<br/>for clinic</a>
+				</div>
+			</div>
+		</div>
+		
+		
+		<div id="digitalTrip">
+			<div data-speed='0.75' class='text-center fullScreen'>
+				<div class="table">
+					<div class="cell">
+						<img class="DT-img" src="https://i.imgur.com/onz9yY8.png" alt="" />
+					</div>
+				</div>
+			</div>
+			<div class="fullScreen text-center" data-speed="1">
+				<div class="table">
+					<div class="cell">
+						<img class="DT-img DT-upperImage" src="https://i.imgur.com/MEU7Miv.png" alt="" />
+					</div>
+				</div>
+			</div>
+			<div data-speed="2" class="fullScreen text-center">
+				<div class="table">
+					<div class="cell">
+						<img class="DT-img DT-logo" src="https://i.imgur.com/n4D1J9K.png" alt="" />
+					</div>
+				</div>
+			</div>
+			<div data-speed='4' class='text-center fullScreen'>
+				<div class="table">
+					<div class="cell">
+						<img class="DT-img DT-frontImg" src="https://i.imgur.com/LEGtjsF.png" alt="" />
+					</div>
+				</div>
+			</div>		
+			<div data-speed='2' class='text-center fullScreen'>
+				<div class="table">
+					<div class="cell">
+						<img class="DT-img" src="https://i.imgur.com/Y8qpthW.png" alt="" />
+					</div>
+				</div>
+			</div>
+			<div class="topCaption">
+				<h2 data-speed="1.6"><a href="#">WebGL game</a></h2>
+			</div>		
+		</div>
+		
+		
+		<div id="kynsi" class="">
+			<div class="fullScreen text-center" data-speed="1">
+				<img class="kynsi-pic" src="https://i.imgur.com/1pQHcou.png" style="position:static !important" alt="" />
+			</div>
+			<div class="topCaption">
+				<h2 data-speed="1.8"><a href="#">Branding & Website</a></h2>
+			</div>
+			<div class="kynsi-text" data-speed="4">
+				<img class="kynsi-text-img" src="https://i.imgur.com/Zv9Faai.png" alt="" />
+			</div>
+		</div>
+		
+			
+		<div id="kis2015">
+			<div class="fullScreen" data-speed="1.6">
+				<div class="table">
+					<div class="cell"><img src="https://i.imgur.com/oxCSpjt.png" class="plLayer kisLayer04" alt="" /></div>
+				</div>
+			</div>
+			<div class="fullScreen" data-speed="2">
+				<div class="table">
+					<div class="cell"><img src="https://i.imgur.com/jOt8Zbl.png" class="plLayer kisLayer03" alt="" /></div>
+				</div>
+			</div>
+			<div class="fullScreen topCaption" data-speed="2">
+				<h2>
+					<a href="#">
+						<img src="https://raw.githubusercontent.com/masumhasan/beelab.github.io/master/BeeLabLLC-logo-306.png" class="kis-logo" alt="" />
+						Promotional<br/>Website
+					</a>
+				</h2>
+			</div>
+			<div class="fullScreen" data-speed="2.5">
+				<div class="table">
+					<div class="cell"><img src="https://i.imgur.com/xgf770I.png" class="plLayer kisLayer02" alt="" /></div>
+				</div>
+			</div>
+			<div class="fullScreen" data-speed="7">
+				<div class="table">
+					<div class="cell"><img src="https://i.imgur.com/P5vV74W.png" class="plLayer kisLayer01" alt="" /></div>
+				</div>
+			</div>		
+			<div class="fullScreen topCaption link-shadow" data-speed="2.5">
+				<div class="h1">
+					<a href="#">
+						<img src="https://raw.githubusercontent.com/masumhasan/beelab.github.io/master/BeeLabLLC-logo-306.png" class="kis-logo" alt="" />
+						Promotional<br/>Website
+					</a>
+				</div>
+			</div>
+		</div>
+		
+		
+		<div id="bilet2u">
+
+			<img data-speed="0.75" src="https://raw.githubusercontent.com/BeeLabLLC/websitefile/master/clients/l-1.png" class="b2u-l-1" alt="" />
+
+			<div data-speed="1" class='fullScreen text-center'>
+				
+				<img src="https://i.imgur.com/yJ2OVyy.png" class="b2u-man" alt="" />
+			</div>
+
+			<img data-speed="1.5" src="https://raw.githubusercontent.com/BeeLabLLC/websitefile/master/clients/l0.png" class="b2u-l0" alt="" />
+			<img data-speed="2.5" src="https://raw.githubusercontent.com/BeeLabLLC/websitefile/master/clients/l1.png" class="b2u-l1" alt="" />
+			
+			<div data-speed='2.5' class='text-center fillParent'>
+				<img src="https://i.imgur.com/CUBsKfq.png" class="bilet2uLogo" alt="" />
+			</div>
+			
+			<div class="topCaption yStack">
+				<h2 data-speed="2.5">Ticket Sales<br/>Website</h2>
+			</div>
+
+			<div data-speed="4" class='b2u-confetti left'>
+				<img src="https://raw.githubusercontent.com/BeeLabLLC/websitefile/master/clients/big_r_speckle.png" class="b2u-r-specle" alt="" />
+			</div>
+			<div data-speed="10" class='b2u-confetti right'>
+				<img src="https://raw.githubusercontent.com/BeeLabLLC/websitefile/master/clients/big_y_speckle.png" class="b2u-y-specle" alt="" />
+			</div>
+
+			<div class="topCaption link-shadow">
+				<div data-speed="2.5" class="h1">
+					<a href="#">Ticket Sales<br/>Website</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	
+	<div class="nav-animation">
+		<img src="https://raw.githubusercontent.com/BeeLabLLC/websitefile/master/img/mouse.gif" alt="scroll" />
+	</div>
+	<a class="right-arrow">
+		<img src="https://raw.githubusercontent.com/BeeLabLLC/websitefile/master/img/arrow_right.png" alt="arrow" />
+	</a>
+	<a class="left-arrow">
+		<img src="https://raw.githubusercontent.com/BeeLabLLC/websitefile/master/img/arrow_left.png" alt="arrow" />
+	</a>
+
+	
+	
+		<div id="footer" class="Footer">
+		<ul class="Footer-menu">
+			
+			<li class="team vAligner">
+				<a class="link-st vItem" href="#">Home</a>
+			</li>
+			<li>
+				<a class="link-st" href="#">Portfolio</a>
+			</li>
+			<li class="location last">
+				<a class="link-st" href="#" target="_blank">contact</a>
+			</li>
+		</ul>
+	</div>	
+	
+	
+	
+	<script type="text/javascript" src="https://firebasestorage.googleapis.com/v0/b/beelab-inc.appspot.com/o/rootsite%2Fjs%2Fbase.js?alt=media&token=c0176c9c-45e0-413b-97fe-f4b53674582c"></script>
+	
+	
+	
+	<script type="text/javascript" src="https://firebasestorage.googleapis.com/v0/b/beelab-inc.appspot.com/o/rootsite%2Fjs%2Findex.js?alt=media&token=011868dc-290e-4116-afcd-575327ca4326"></script>
+
+</body> 
+
+</html>
